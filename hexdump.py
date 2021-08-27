f0465f5f378170e999b034aeb81e97cc84c2b599
Akihiro <36966603+GlowingObsidian@users.noreply.github.com>
2021-08-27 09:27:38 +0530

    Added better line formatting and overall bug fixes

--- hexdump.py
+++ hexdump.py
@@ -1,9 +1,13 @@
+#Copyright(C)2021
+#Made by Akihiro
+#Version 2.0
+
 import sys
 
 f=open(sys.argv[1],"rb")
 file=list(f.read())
 f.close()
-file.append(0)
+file.append(0) #file terminator
 
 prev=list()
 curr=list()
@@ -19,18 +23,27 @@ for i in file:
                 buffer.append('*')
         else:
             buff_hex=''
-            for j in curr:
-                buff_hex=buff_hex+hex(j)+'\t'
+            for j in range(0,len(curr)):
+                space=''
+                if(j!=0 and (j+1)%8==0):
+                    space='  '
+                else:
+                    space=' '
+                buff_hex=buff_hex+'00'[:-len(hex(curr[j])[2:])]+hex(curr[j])[2:]+space #hex fomatting
             buff_str='|'
-            for j in curr:
-                buff_str=buff_str+chr(j)
+            for j in curr: #string formatting
+                if(j>31 and j<127):
+                    buff_str=buff_str+chr(j)
+                else:
+                    buff_str=buff_str+'.' 
             buff_str=buff_str+'|'
-            buffer.append(hex(count-16)+'\t'+buff_hex+buff_str)
+            buffer.append('00000000'[:-len(hex(count-16)[2:])]+hex(count-16)[2:]+'  '+buff_hex+buff_str) #adding to buffer
         prev=curr
         curr=list()
     count=count+1
     curr.append(i)
-buffer.append(hex(count-1))
+buffer.append('00000000'[:-len(hex(count-16)[2:])]+hex(count-1)[2:])
 
+print()
 for i in buffer:
     print(i)
