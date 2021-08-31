0156809dcb98ed825d0cab8c87d4f7f880de3a6f
Akihiro <36966603+GlowingObsidian@users.noreply.github.com>
2021-08-31 12:59:25 +0530

    Bug fixes and size view.

--- hexdump.py
+++ hexdump.py
@@ -1,8 +1,11 @@
 #Copyright(C)2021
 #Made by Akihiro
-#Version 2.5
+#Version 3.0
 
 import sys
+
+def format(alignment,default,word):
+    None
 
 f=open(sys.argv[1],"rb")
 file=list(f.read())
@@ -10,15 +13,11 @@ f.close()
 file.append(0) #file terminator
 
 prev=list()
-curr=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-pos=0
+curr=list()
 count=0
 buffer=list()
 
 for i in file:
-    if pos == 16:
-        pos=0
-        
     if(count!=0 and (count%16==0 or count==len(file)-1)):
         if(prev==curr):
             if(buffer[len(buffer)-1]=='*'):
@@ -34,21 +33,26 @@ for i in file:
                 else:
                     space=' '
                 buff_hex=buff_hex+'00'[:-len(hex(curr[j])[2:])]+hex(curr[j])[2:]+space #hex fomatting
-            buff_str='|'
+            for k in range(0,50-len(buff_hex)):
+                buff_hex=buff_hex+' '
+            buff_str=''
             for j in curr: #string formatting
                 if(j>31 and j<127):
                     buff_str=buff_str+chr(j)
                 else:
                     buff_str=buff_str+'.' 
-            buff_str=buff_str+'|'
+            for k in range(0,16-len(buff_str)):
+                buff_str=buff_str+' '
+            buff_str='|'+buff_str+'|'
             buffer.append('00000000'[:-len(hex(count-(count%16)-(0 if count%16!=0 else 16))[2:])]+hex(count-(count%16)-(0 if count%16!=0 else 16))[2:]+'  '+buff_hex+buff_str) #adding to buffer
         prev=curr
-        curr=curr=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        curr=list()
     count=count+1
-    curr[pos]=i
-    pos=pos+1
+    curr.append(i)
 buffer.append('00000000'[:-len(hex(count-16)[2:])]+hex(count-1)[2:])
 
 print()
 for i in buffer:
     print(i)
+
+print('\n'+'Size: '+str((len(file)-1)/1024)+' KB')
